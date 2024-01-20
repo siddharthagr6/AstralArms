b46e0d9f0d3afd429eb2d6ca0253d0787489617e
Siddharth Agrawal <siddharth.agr6@gmail.com>
2024-01-20 10:38:29 +0530

    Initial Commit

---
+++ AstralArms/AstralArmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstralArmsApp.swift
+//  AstralArms
+//
+//  Created by Testuser on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AstralArmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
